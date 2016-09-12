742c455c51e1d5bfa1338ace22e4c6e159f87ede
songyu <syiosengineer1992@gmail.com>
2016-09-12 17:20:35 +0800

    脚本迁移初始化

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from . import db
 
 
 class Role(db.Model):
@@ -32,6 +35,26 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SERCET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SERCET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
